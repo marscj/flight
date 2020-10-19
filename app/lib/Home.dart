ba12eeaa3d6c7070d59ab7c2c2743406d4eced5e
marscj <mjx_cj@163.com>
2020-10-19 17:32:50 +0400

    commit

---
+++ app/lib/Home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  Home({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Stack(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
