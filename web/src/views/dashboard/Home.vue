74e7ebd02c912737e2465c40ecf361c4baef1eb6
marscj <mjx_cj@163.com>
2020-11-23 14:11:32 +0400

    commit

---
+++ web/src/views/dashboard/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <page-header-wrapper> </page-header-wrapper>
+</template>
+
+<script>
+import { PageHeaderWrapper } from '@ant-design-vue/pro-layout'
+
+export default {
+  components: {
+    PageHeaderWrapper
+  }
+}
+</script>
+
+<style></style>
