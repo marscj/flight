ea7b39aae7b77dcaac24040c78192b6b43fb35c8
marscj <mjx_cj@163.com>
2020-12-14 11:41:25 +0400

    commit

---
+++ web/dist/js/chunk-2d21b0a8.7608563b.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21b0a8"],{bda1:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("index",{attrs:{post_type:"add"}})},s=[],c=t("6991"),o={components:{Index:c["a"]}},u=o,d=t("2877"),l=Object(d["a"])(u,a,s,!1,null,null,null);e["default"]=l.exports}}]);
