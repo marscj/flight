28ecad504bd1eb6088813b248bf9c88559399bf8
marscj <mjx_cj@163.com>
2021-01-17 19:21:15 +0400

    commit

--- server/app/views.py
+++ server/app/views.py
@@ -26,7 +26,7 @@ class CheckVersion(APIView):
                     return Response({'result': True, 'url': app.redirect})
                 else:
                     if app.file is not None:
-                        return Response({'result': True, 'url': app.app.file.url}) 
+                        return Response({'result': True, 'url': app.file.url}) 
                     else:
                         return Response({'result': True})
         
