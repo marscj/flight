c8309dd7eaa430ff3bd4cb905d894052ac47f099
marscj <ifdefine@cshape.com>
2021-03-07 16:07:50 +0400

    commit

--- server/ticket/signals.py
+++ server/ticket/signals.py
@@ -27,14 +27,16 @@ def post_create_historical_record_callback(sender, instance, history_instance, h
     serializer = None
 
     if history_instance.history_type == '-':
-        instance.messages.all().delete()
-        instance.comments.all().delete()
             
         if type(instance).__name__ == 'Booking':
             instance.itineraries.all().delete()
+            instance.messages.all().delete()
+            instance.comments.all().delete()
 
         if type(instance).__name__ == 'Ticket':
             instance.children.all().delete()
+            instance.messages.all().delete()
+            instance.comments.all().delete()
         
         return 
     
