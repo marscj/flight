9397c30a0c30c12881b17e39695ba36f740b484d
marscj <ifdefine@cshape.com>
2021-03-08 00:11:20 +0400

    commit

--- server/ticket/models.py
+++ server/ticket/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.db.models import Q
 from django.db.models import Value
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
